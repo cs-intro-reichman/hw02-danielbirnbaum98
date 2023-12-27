67d5bcb7d75dae5f37b0301bbc8fb8ea447654d5
Daniel <daniel.birnbaum@post.runi.ac.il>
2023-12-27 17:06:22 +0200

    Fisrt 4 + 5

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -1,8 +1,21 @@
-/**
- *  Gets a command-line argument n (int), and prints an n-by-n damka board.
- */
+
 public class DamkaBoard {
 	public static void main(String[] args) {
-		//// Put your code here
+		int n = Integer.parseInt(args[0]);
+		int i = 0;
+		while (i < n) { 
+			int j = 0;
+			while(j < n){
+				if (i % 2 == 0){
+				System.out.print("* ");
+				}
+				else{
+				System.out.print(" *");
+				}
+				j++;
+			}
+			System.out.println();
+			i++;
+		}
 	}
 }
